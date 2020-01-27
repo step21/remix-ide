a702b75cb60e1baa8e4e8066fc50ff54f4a0d0b0
step21 <step21@devtal.de>
2020-01-27 23:05:02 +0100

    Add lexon to landing page

--- src/app/ui/landing-page/landing-page.js
+++ src/app/ui/landing-page/landing-page.js
@@ -129,6 +129,11 @@ export class LandingPage extends ViewPlugin {
       this.appManager.ensureActivated('udapp')
       this.verticalIcons.select('vyper')
     }
+    const startLexon = () => {
+      this.appManager.ensureActivated('solidity')
+      this.appManager.ensureActivated('udapp')
+      this.verticalIcons.select('lexon')
+    }
     const startWorkshop = () => {
       this.appManager.ensureActivated('box')
       this.appManager.ensureActivated('solidity')
@@ -196,6 +201,7 @@ export class LandingPage extends ViewPlugin {
             <div class="${css.enviroments} pt-2">
               <button class="btn btn-lg btn-secondary mr-3" onclick=${() => startSolidity()}>Solidity</button>
               <button class="btn btn-lg btn-secondary mr-3" onclick=${() => startVyper()}>Vyper</button>
+              <button class="btn btn-lg btn-secondary mr-3" onclick=${() => startLexon()}>Lexon</button>
             </div>
           </div>
           <div class="file">

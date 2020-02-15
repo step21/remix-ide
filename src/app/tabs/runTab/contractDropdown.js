f16c2d1c7a34a27047b00ecdaf82f9f9128ff788
step21 <step21@devtal.de>
2020-02-15 16:35:27 +0100

    Modifications for event debugging and lexon plugin

--- src/app/tabs/runTab/contractDropdown.js
+++ src/app/tabs/runTab/contractDropdown.js
@@ -22,6 +22,7 @@ class ContractDropdownUI {
     this.dropdownLogic.event.register('newlyCompiled', (success, data, source, compiler, compilerFullName, file) => {
       if (!document.querySelector(`.${css.contractNames.classNames[0]}`)) return
       var contractNames = document.querySelector(`.${css.contractNames.classNames[0]}`)
+      console.log('contract names', contractNames)
       contractNames.innerHTML = ''
       if (success) {
         this.selectContractNames.removeAttribute('disabled')
